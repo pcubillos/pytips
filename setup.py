f2727ac5463b2f511e32775b800cf6fca38ec15c
pcubillos <pcubillos@knights.ucf.edu>
2015-07-09 01:05:41 -0400

    Reformated the setup.py and moved header files to include/

--- setup.py
+++ setup.py
@@ -2,23 +2,33 @@ from numpy import get_include
 import os, re, sys
 from distutils.core import setup, Extension
 
-srcdir = 'src/'  # C-code source folder
-libdir = 'lib/'  # Where the shared objects are put
+srcdir = 'src/'    # C-code source folder
+incdir = 'include' # Include folder with header files
+#libdir = 'lib/'    # Where the shared objects are put
 
 files = os.listdir(srcdir)
 # This will filter the results for just the c files:
 files = filter(lambda x:     re.search('.+[.]c$',     x), files)
 files = filter(lambda x: not re.search('[.#].+[.]c$', x), files)
 
-ext_mod = []
-inc = [get_include()]
+inc = [get_include(), incdir]
+eca = []  # ['-fopenmp']
+ela = []  # ['-lgomp']
 
+extensions = []
 for i in range(len(files)):
-  exec("mod{:d} = Extension('{:s}', sources=['{:s}{:s}'], include_dirs=inc, "
-       "extra_compile_args=['-fopenmp'], extra_link_args=['-lgomp'])".format(
-       i, files[i].rstrip('.c'), srcdir, files[i]))
+  e = Extension(files[i].rstrip('.c'),
+                sources=["{:s}{:s}".format(srcdir,files[i])],
+                include_dirs=inc,
+                extra_compile_args=eca,
+                extra_link_args=ela)
+  extensions.append(e)
 
-  exec('ext_mod.append(mod{:d})'.format(i))
 
-setup(name=libdir, version='1.0', description='c extension functions',
-      ext_modules = ext_mod)
+setup(name          = "CTIPS",
+      version       = '1.0.0',
+      author        = "Patricio Cubillos",
+      author_email  = "pcubillos@fulbrightmail.org",
+      url           = "https://github.com/pcubillos/ctips",
+      description   = 'Partition Function Calculator',
+      ext_modules   = extensions)
