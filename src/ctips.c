5f79d4c686e2366b27cdd9b89218ff3ee6c1e3bf
AJ Foster <web@aj-foster.com>
2015-07-24 18:49:41 -0400

    Update to be compatible with Numpy 1.8 API.

--- src/ctips.c
+++ src/ctips.c
@@ -1,4 +1,6 @@
 #include <Python.h>
+
+#define NPY_NO_DEPRECATED_API NPY_1_8_API_VERSION
 #include <numpy/arrayobject.h>
 
 #include "ind.h"
@@ -72,7 +74,7 @@ static PyObject *iso(PyObject *self, PyObject *args){
   size[0] = niso[mol];
 
   /* Fill in isotope ID's for this molecule:                                */
-  iso = (PyArrayObject *) PyArray_SimpleNew(1, size, PyArray_INT);
+  iso = (PyArrayObject *) PyArray_SimpleNew(1, size, NPY_INT);
   for (i=0; i<niso[mol]; i++){
     INDi(iso, i) = isoID[cumiso[mol]+i];
   }
@@ -111,11 +113,11 @@ static PyObject *tips(PyObject *self, PyObject *args){
     return NULL;
 
   /* Get array size:                                                        */
-  ndata = molID->dimensions[0];
+  ndata = PyArray_DIM(molID, 0);
   size[0] = ndata;
 
   /* Returned array with the partition function:                            */
-  Qarray = (PyArrayObject *) PyArray_SimpleNew(1, size, PyArray_DOUBLE);
+  Qarray = (PyArrayObject *) PyArray_SimpleNew(1, size, NPY_DOUBLE);
 
   /* Calculate the partition function for each input value:                 */
   for (i=0; i<ndata; i++){
