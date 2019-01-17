deb714dba4a2e5a7b7278b5e580b5bfbd04610be
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2019-01-17 11:10:59 +0100

    Updated copyright year.

--- src_c/include/lagrange.h
+++ src_c/include/lagrange.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2018 Patricio Cubillos and contributors.
+// Copyright (c) 2015-2019 Patricio Cubillos and contributors.
 // pytips is open-source software under the MIT license (see LICENSE).
 
 /* Lagrange interpolation routines:                                         */
